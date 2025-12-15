fa80615c792174733e6fb6da62662de704046c69
BuildTools <chaserichards242@gmail.com>
2025-12-15 00:50:30 -0600

    Changes names

--- src/main/java/ItsGh0ul/Better_Than_Stairs/BetterThanStairs.java
+++ src/main/java/ItsGh0ul/Better_Than_Stairs/BetterThanStairs.java
@@ -1,4 +1,4 @@
-package turniplabs.examplemod;
+package ItsGh0ul.Better_Than_Stairs;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -6,8 +6,8 @@ import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
-public class ExampleMod implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
-	public static final String MOD_ID = "examplemod";
+public class BetterThanStairs implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
+	public static final String MOD_ID = "betterthanstairs";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	@Override
 	public void onInitialize() {
